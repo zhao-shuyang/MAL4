a76baa2690ff27e528ba18b2afb7887c9137d48d
Shuyang Zhao <zhaos@narvi.cc.tut.fi>
2018-11-28 14:58:43 +0200

    manage to run

--- extract_wav.py
+++ extract_wav.py
@@ -2,13 +2,13 @@ import sys
 import csv
 import subprocess
 
-audio_folder = "wav"
+audio_folder = "/home/zhaos/Audioset_wav"
 
-with open('meta/balanced_train_segments.csv', 'rb') as csvfile:
+with open('meta/balanced_train_segments.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for item in reader:
         print(item)
         command = """
-        youtube-dl https://www.youtube.com/watch?v=%s --exec "ffmpeg -y -ss %s -i {} -acodec %s -ar %d -ac 1 -t 10 %s/{}.wav; rm {};"
-        """%(item['YTID'], item['start_seconds'],"pcm_s16le", 44100, audio_folder)
+        youtube-dl -f bestaudio https://www.youtube.com/watch?v=%s --exec "ffmpeg -y -ss %s -i {} -acodec %s -ar %d -ac 1 -t 10 %s/{}.wav && rm {} && echo {},%s >> meta/wavfiles.csv;";
+        """%(item['YTID'], item['start_seconds'],"pcm_s16le", 44100, audio_folder, item['YTID'])
         subprocess.call(command, shell=True)
